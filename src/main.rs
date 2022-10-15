7cd658eb4644a706bc62702d69e7a4481bc48c97
Samuel Cristobal <el.tobal@gmail.com>
2022-10-15 21:54:42 +0200

    fixed tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use craby::craby_r8::CrabyBot;
 use std::io::Result;
-use tokio::main as async_main;
 
-#[async_main]
+#[tokio::main]
 async fn main() -> Result<()> {
-    let bot = CrabyBot::new_from_env();
+    tokio::spawn(async move {
+        let bot = CrabyBot::new_from_env();
+        bot.run().await
+    });
 
-    tokio::spawn(async move { bot.run().await });
+    tokio::spawn(async move {
+        let sever = craby::webhooks::new_server().expect("Failed to start webhook server");
+        sever.await
+    });
 
-    let sever = craby::webhooks::new_server().expect("Failed to start webhook server");
-
-    tokio::spawn(async move { sever.await });
-
-    Ok(())
+    tokio::signal::ctrl_c().await
 }
