6f9fbd0c69a1c6f0b895f406ea198be994258e15
Samuel Cristobal <el.tobal@gmail.com>
2023-03-12 09:07:51 +0100

    wip native futures handling in warp

--- src/webhook_server.rs
+++ src/webhook_server.rs
@@ -1,9 +1,12 @@
 use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 use tokio::sync::{oneshot, Mutex};
+use tracing::info;
 use warp::{hyper::body::Bytes, Filter};
 
+type LookupTable = Arc<Mutex<HashMap<String, oneshot::Sender<Bytes>>>>;
+
 pub struct WebhookServer {
-    tx_results: Arc<Mutex<HashMap<String, oneshot::Sender<Bytes>>>>,
+    tx_results: LookupTable,
 }
 
 impl WebhookServer {
@@ -14,28 +17,26 @@ impl WebhookServer {
     pub async fn run(self, addr: impl Into<SocketAddr>) {
         let use_tx_results = warp::any().map(move || Arc::clone(&self.tx_results));
 
-        let process_entry =
-            |id: String,
-             body: Bytes,
-             tx_map: Arc<Mutex<HashMap<String, oneshot::Sender<Bytes>>>>| {
-                log::info!("webhook received: {}", id);
+        let process_entry = |id: String, body: Bytes, tx_map: LookupTable| async move {
+            info!("webhook received: {}", id);
 
-                tokio::spawn(async move {
-                    let tx_map = &mut tx_map.lock().await;
-                    let tx = tx_map.remove(&id);
+            let tx_map = &mut tx_map.lock().await;
+            let tx = tx_map.remove(&id);
 
-                    tx.and_then(|tx| tx.send(body).ok());
-                });
+            let res = tx.and_then(|tx| tx.send(body).ok());
 
-                warp::http::StatusCode::OK
-            };
+            match res {
+                Some(_) => Ok(warp::http::StatusCode::OK),
+                None => Err(warp::reject::not_found()),
+            }
+        };
 
         let webhooks = warp::post()
             .and(warp::path!("webhook" / String))
             .and(warp::body::content_length_limit(1024 * 16))
             .and(warp::body::bytes())
             .and(use_tx_results)
-            .map(process_entry);
+            .and_then(process_entry);
 
         let health = warp::get()
             .and(warp::path!("health-check"))
